8c3bbbbcfc3e0cbb2883fdf536213a9ec6fe4afe
treckstar <treckstar@gmail.com>
2022-09-19 22:41:36 -0400

    fix: tailwind stuff

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,27 +1,12 @@
-const colors = require("tailwindcss/colors")
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        neutral: colors.neutral,
-        primary: colors.sky,
-      },
-    },
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: "1rem",
-        xs: "1rem",
-        sm: "2rem",
-        xl: "5rem",
-        "2xl": "6rem",
-      },
-    },
-  },
-  plugins: [
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/typography"),
+  content: [
+    "./src/pages/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/constants/*.{js,jsx,ts,tsx}"
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
