9e659e6ada477a7395b1caa9af57bf0afdde973a
treckstar <treckstar@gmail.com>
2022-09-19 22:45:33 -0400

    fix: article template

--- frontend/src/templates/article-post.js
+++ frontend/src/templates/article-post.js
@@ -4,8 +4,11 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import BlocksRenderer from "../components/blocks-renderer"
 import Seo from "../components/seo"
+//import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader'
 
 const ArticlePage = ({ data }) => {
+  //deckDeckGoHighlightElement()
+
   const article = data.strapiArticle
 
   const seo = {
@@ -17,26 +20,32 @@ const ArticlePage = ({ data }) => {
   return (
     <Layout as="article">
       <Seo seo={seo} />
-      <header className="container max-w-4xl py-8">
-        <h1 className="text-6xl font-bold text-neutral-700">{article.title}</h1>
-        <p className="mt-4 text-2xl text-neutral-500">{article.description}</p>
-        <GatsbyImage
-          image={getImage(article?.cover?.localFile)}
-          alt={article?.cover?.alternativeText}
-          className="mt-6"
-        />
-      </header>
-      <main className="mt-8">
-        <BlocksRenderer blocks={article.blocks || []} />
-      </main>
+      <section className="blog-template">
+        <div className="section-center">
+          <header className="container max-w-4xl py-8">
+            <h1 className="text-6xl font-bold text-neutral-700">{article.title}</h1>
+            <p className="mt-4 text-2xl text-neutral-500">{article.description}</p>
+            <GatsbyImage
+              image={getImage(article?.cover?.localFile)}
+              alt={article?.cover?.alternativeText}
+              className="mt-6"
+            />
+          </header>
+          <article className="blog-content">
+            <BlocksRenderer blocks={article.blocks || []} />
+          </article>
+        </div>
+      </section>
     </Layout>
-  )
+  );
+
 }
 
 export const pageQuery = graphql`
   query ($slug: String) {
     strapiArticle(slug: { eq: $slug }) {
       id
+      strapi_id
       slug
       title
       description
@@ -47,9 +56,7 @@ export const pageQuery = graphql`
         alternativeText
         localFile {
           url
-          childImageSharp {
-            gatsbyImageData
-          }
+          publicURL
         }
       }
     }
