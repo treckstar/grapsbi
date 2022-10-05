e825bcba8b2668769ec67fc3d3b0eab915dfd083
treckstar <treckstar@gmail.com>
2022-10-04 21:53:49 -0400

    fix: missing ) after argument list

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST"),
       port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME",
+      database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
       ssl: {
