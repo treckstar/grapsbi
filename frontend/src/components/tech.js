f1aeeb3e798a620205194c010674267a5360f813
treckstar <treckstar@gmail.com>
2023-03-20 09:12:33 -0400

    fix: add new tech
    Signed-off-by: treckstar <treckstar@gmail.com>

--- frontend/src/components/tech.js
+++ frontend/src/components/tech.js
@@ -1,20 +1,44 @@
 import React from 'react'
-import { Image } from 'gatsby'
+
+import { StaticImage, getSrc, GatsbyImage} from 'gatsby-plugin-image'
 import cx from 'classnames'
-import Vue3Icon from '../assets/icons/vue.inline.svg'
 
 import { motion } from 'framer-motion'
 import {
     showHoverAnimation,
-    removeHoverAnimation,
+    removeHoverAnimation
 } from '../lib/windowAnimation'
 import { FadeContainer, popUp } from '../lib/FramerMotionVariants'
-
+import VueIcon from '../assets/icons/vue.inline.svg'
+import ReactIcon from '../assets/icons/react.inline.svg'
+import VercelIcon from '../assets/icons/vercel.inline.svg'
+import TailwindCssIcon from '../assets/icons/tailwindcss.inline.svg'
+import TypescriptIcon from '../assets/icons/typescript.inline.svg'
 const technology = [
     {
         name: 'Vue 3',
         desc: 'Vue 3 and its powerful composition API makes your experience pleasant.',
-        icon: '../assets/icons/vue.inline.svg',
+        icon: <VueIcon width="64" height="64" />,
+    },
+    {
+        name: 'React',
+        desc: 'React ',
+        icon: <ReactIcon width="64" height="64" />,
+    },
+    {
+        name: 'Next',
+        desc: 'Next js  ',
+        icon: <VercelIcon width="64" height="64" />,
+    },
+    {
+        name: 'Tailwind CSS',
+        desc: 'Tailwind CSS',
+        icon: <TailwindCssIcon width="64" height="64" />,
+    },
+    {
+        name: 'Typescript',
+        desc: 'Typescript',
+        icon: <TypescriptIcon width="64" height="64" />,
     },
     // More people...
 ]
@@ -35,11 +59,10 @@ export default function Skills() {
                     whileInView="visible"
                     variants={FadeContainer}
                     viewport={{ once: true }}>
-                    
                     {technology.map((tech) => (
                         <motion.li
                             key={tech.name}
-                            title={tech.name}
+                            ss
                             variants={popUp}
                             key={tech.name}
                             onMouseMove={(e) => showHoverAnimation(e)}
@@ -47,12 +70,12 @@ export default function Skills() {
                             className="bg-teal-800 hover:bg-teal-700 dark:bg-darkPrimary group origin-center transform items-center justify-center gap-4 border-solid rounded-2xl hover:border-white dark:border-neutral-700 hover:rounded-2xl hover:dark:bg-darkSecondary p-4">
                             <div className="bg-transparent hover:border-teal-900 pointer-events-none relative select-none transition group-hover:scale-110 sm:group-hover:scale-100  p-4">
                                 <div className="bg-transparent mx-auto">
-                                    <Vue3Icon width="64" height="64" />
+                                    {tech.icon}
                                 </div>
-                                <h3 className="bg-transparent mt-6 font-poppins font-semibold leading-7 tracking-tight text-white text-lg">
+                                <h3 className=" bg-gradient-to-b from-slate-300 to-slate-200 mb-8 bg-clip-text tracking-tight text-transparent bg-transparent mt-6 font-poppins font-semibold text-md">
                                     {tech.name}
                                 </h3>
-                                <p className="bg-transparent text-sm leading-6 text-gray-400">
+                                <p className="bg-gradient-to-b from-gray-300 to-gray-200 mb-8 bg-clip-text tracking-tight text-transparent bg-transparent mt-6 font-poppins font-semibold text-sm">
                                     {tech.desc}
                                 </p>
                             </div>
